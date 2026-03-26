651c93145a6d7a9b8022c67be53565b6637aa249
Pedro Ferreira Baccelli Reis <pedrofbr@hotmail.com>
2026-03-26 14:54:09 -0300

    feat: add Portuguese (Brazil) locale support to Sanity configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import { ptBRLocale } from '@sanity/locale-pt-br'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: 'bfw6ro7b',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), ptBRLocale()],
 
   schema: {
     types: schemaTypes,
